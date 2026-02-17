ed230425f0e7e0f3aa184ab9b3a4edd6ec57fc66
PURPLECT <calvinthao3@gmail.com>
2026-02-16 21:20:57 -0600

    I removed stuff that wasn't important

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,10 +1,5 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import static frc.robot.Constants.Intake.INTAKEMOTOR1_CANID;
-import static frc.robot.Constants.Intake.INTAKEMOTOR2_CANID;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import lib.NiceSubsytem;
 
