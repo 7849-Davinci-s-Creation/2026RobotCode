cda967215936e9958abc8160faf6bcd1af745439
v0ncent <83373495+v0ncent@users.noreply.github.com>
2026-02-17 00:00:52 -0600

    unused

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -44,9 +44,7 @@ public final class Constants {
     }
 
     public static final class Intake {
-        // FIND THE CAN IDS WHEN WE GET THE ROBOT WORKING
-        public static final int INTAKEMOTOR1_CANID = 3;
-        public static final int INTAKEMOTOR2_CANID = 4;
+
     }
 
     public static final class Vision {
